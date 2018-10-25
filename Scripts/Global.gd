7e12f692d6e811f1c72a2594540eee4bbac7bb83
Unknown <dani_mrx93@hotmail.com>
2018-10-24 22:26:02 -0300

    + Add coin chimi and rework movement

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var camera
-var player
+var camera = null
+var player = null
 var coins = 0
+var UI = null
 
 func _ready():
 	pass
---
+++ Scripts/UI.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+var meters = 0
+var coins = 0
+var coinLayer
+
+func _ready():
+	Global.UI = self
+	$Panel/Coins.text = str(Global.coins)
+	$Panel/Meters.text = str(Global.player.get_meters()) + "m"
+	coinLayer = $CoinLayer
+	pass
+
+func _process(delta):
+	update_labels()
+	pass
+
+func update_labels():
+	if meters != Global.player.get_meters():
+		meters = Global.player.get_meters()
+		$Panel/Meters.text = str(int(meters)) + "m"
+	if coins != Global.coins:
+		coins = Global.coins
+		$Panel/Coins.text = str(coins)
+	pass
