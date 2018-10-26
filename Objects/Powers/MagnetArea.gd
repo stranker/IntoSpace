3b7341e3bdde7f30c0672ce2a36142fe8bd38c60
Unknown <dani_mrx93@hotmail.com>
2018-10-26 19:53:31 -0300

    + Add magnet sprite

--- Objects/Powers/MagnetArea.gd
+++ Objects/Powers/MagnetArea.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 export (float) var incrementFactor = 5
 export (float) var initialRadius = 50
+var angle = 0
 
 func _ready():
 	add_to_group("MagneticArea")
 	$Colision.shape.radius = initialRadius
 
+func _process(delta):
+	angle += delta * 5
+	rotation = angle
+
 func increment_magnet():
 	$Colision.shape.radius += incrementFactor
+	$Sprite.scale += Vector2(0.15,0.15)
 	pass
