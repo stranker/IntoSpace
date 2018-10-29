99076551e9615ccbedd83b26c2c7c359a38268c1
Unknown <dani_mrx93@hotmail.com>
2018-10-29 18:42:17 -0300

    + Add Scout Enemy

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+export (int) var life = 0
+export (int) var speed = 0
+export (int) var damage = 0
+var velocity = Vector2()
+var direction = Vector2()
+
+func _ready():
+	pass
+
+func take_damage(damage):
+	pass
